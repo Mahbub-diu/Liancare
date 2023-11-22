27907892d744593a5d882f930e067e8d70969d6f
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-11-22 19:12:41 +0600

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,27 @@ $('.hamburger-icon').click(function () {
 
 // AOS animation initialized
 AOS.init();
+
+// agendi page contact form
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict';
+
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
